3818fa854ca45fd8b00aa2552c485431ec050ea8
sourceperl <loic.celine@free.fr>
2017-11-10 21:55:47 +0100

    set version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pySupRST',
-    version='0.1.0',
+    version='0.1.1',
     description='little SCADA system',
     author='Loic Lefebvre',
     author_email='loic.celine@free.fr',
