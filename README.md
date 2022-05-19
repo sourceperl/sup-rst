4c95917c646b78911b10794d6fc22928951ca5f5
sourceperl <loic.celine@free.fr>
2022-05-19 19:56:34 +0200

    remove scripts/ from package, set it to v0.1.4

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 # pySupRST
 A simple SCADA system for Python
 
-## Add packages
+## Setup
+
+
 ```bash
+# add packages
 sudo apt install -y nmap supervisor python3-setuptools python3-pymysql \
                     python3-requests python3-schedule
-```
-
-## Add pySupRST
-```bash
-sudo python setup.py install
+# add pySupRST package
+sudo python3 setup.py install
+# copy scripts
+sudo cp -v scripts/* /usr/local/bin/
 ```
 
 ## Supervisor setup
 ```bash
-sudo cp etc/supervisor/conf.d/sup_rst.conf /etc/supervisor/conf.d/
+sudo cp -v etc/supervisor/conf.d/sup_rst.conf /etc/supervisor/conf.d/
 ```
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pySupRST',
-    version='0.1.3',
+    version='0.1.4',
     description='little SCADA system',
     author='Loic Lefebvre',
     author_email='loic.celine@free.fr',
@@ -11,19 +11,6 @@ setup(
     platforms='any',
     py_modules=[
         'pySupRST',
-    ],
-    scripts=[
-        'scripts/srst-db-jobs-srv',
-        'scripts/srst-icmp-srv',
-        'scripts/srst-mbus-srv',
-        'scripts/srst-mbus-exp-srv',
-        'scripts/srst-rest-srv',
-        'scripts/srst_check_tables',
-        'scripts/srst_ls_host',
-        'scripts/srst_rm_host',
-        'scripts/srst_search_host',
-        'scripts/srst_sql_builder',
-        'scripts/srst_whois_host',
     ],
     install_requires=[
         'schedule',
